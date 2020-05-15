9783c54b093ae6d448408c2407b9fe8838ee76d7
= <=>
2020-05-15 10:35:15 -0300

    Product card

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { LocalUser } from "../models/local_user";
 import { STORAGE_KEYS } from "../config/storage_key.config";
+import { Cart } from "../models/cart";
 
 @Injectable()
 export class StorageService{
@@ -17,5 +18,19 @@ export class StorageService{
     if(!obj)
       localStorage.removeItem(STORAGE_KEYS.localUser);
     localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
+  }
+
+  getCart(): Cart {
+    let cart = localStorage.getItem(STORAGE_KEYS.cart);
+    if (!cart)
+      return null;
+
+    return JSON.parse(cart);
+  }
+
+  setCart(obj: Cart) {
+    if (!obj)
+      localStorage.removeItem(STORAGE_KEYS.cart);
+    localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
   }
 }
